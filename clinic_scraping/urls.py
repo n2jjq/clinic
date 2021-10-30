94202e33e5ffa149b971a768f2caf3d19d9b2f0a
Sho Ando <spdtas0223@gmail.com>
2021-10-31 00:05:53 +0900

    urls.pyの修正

--- clinic_scraping/urls.py
+++ clinic_scraping/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     # 一覧画面
     path('', views.scraping, name='index'),
-]
+] +static(settings.IMAGE_URL, document_root=settings.IMAGE_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
